198a12e1740d4163f4d754f7dbba114487c040df
Vert Reactor <r@vert.bg>
2015-03-21 01:12:27 +0200

    This is recursive implementation of binary search in Ruby
    Today I have learned another beautiful usage of recursion. Binary search made easy in Ruby.

---
+++ binary_search_racursive.rb
@@ -0,0 +1,21 @@
+# This is binary search  (recursive)
+# It is implemented like method of the standart Array class
+
+class Array
+  def binary_search(val, left = 0, right = nil)
+  
+  right = self.size unless right
+  
+  mid = (left + right) / 2
+  
+  return nil if left > right
+  
+    if val == self[mid]
+      return mid
+    elsif val > self[mid]
+      binary_search(val, mid + 1, right)
+    else
+      binary_search(val, left, mid - 1)
+    end
+  end
+end  
